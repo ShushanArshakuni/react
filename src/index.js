3799dcebd61cf0a5dfeab460b0b4400897091bfd
tarshakuni <tigranarshakuni1991@gmail.com>
2022-10-05 19:47:02 +0400

    restored counter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./components/wrapper/index.css";
-import App from "./App2";
+import "./components/counter/index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
